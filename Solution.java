bf88a11ab28cbf14b84b875a06f4d30e51042dcb
sdim-ua <ingersol@yandex.ua>
2019-06-18 12:43:37 +0300

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,19 @@
+
+/* 
+«Исправь код», часть 2
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static abstract  class Pet {
+        public String getName() {
+            return "Я - котенок";
+        }
+
+        public abstract  Pet getChild();
+    }
+
+}
